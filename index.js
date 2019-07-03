c5fe6f6975d6db9c04c643208da25f9c54ac2426
Andrew Waller <andrewhwaller@gmail.com>
2019-07-03 22:28:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (someValue) {
- let distanceFromHqInBlocks === someValue
+ let distanceFromHqInBlocks = someValue
 }
 
 function distanceFromHqInFeet (someValue) {
