a9184472d3992757ecefa289e236c0d11bfbf524
Andrew Waller <andrewhwaller@gmail.com>
2019-07-03 22:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (someValue) {
- let distanceFromHqInBlocks = someValue
+ if (someValue > 42) {
+   return someValue - 42;
+ } else {
+   return 42 - someValue;
+ }
 }
 
 function distanceFromHqInFeet (someValue) {
