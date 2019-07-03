118bbf8e69a189e293c9ac07852a74c383494204
Andrew Waller <andrewhwaller@gmail.com>
2019-07-03 22:40:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,15 @@ function distanceTravelledInFeet (start, end) {
 }
 
 function calculatesFarePrice (start, end) {
+  const distance = distanceTravelledInFeet(start, end);
   
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400)
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else if (distance > 2500) {
+    return 'cannot travel that far'
+  }
 }
